3370fc3ab1d5aa655c094151a6ca7f5bc0d52a67
Rachel Jackson <rachelnjackson@yahoo.com>
2018-01-14 13:03:47 -0500

    added additional selects for dropdown options

--- src/MadLibs/MadLibs.js
+++ src/MadLibs/MadLibs.js
@@ -8,20 +8,30 @@ class MadLibs extends React.Component {
     //State is going to hold the words the user selects
     this.state = {
       nouns: [],
-      verbs: [], 
-      adject: []
+      verbs: [],
+      adj: []
     };
   }
 
 
   //componentDidMount
-    //This will get the words from API calls
-  
-  //
+  //This will get the words from API calls
+
+  //user selects words from dropdown boxes
+  //selected words update the appropriate state
   const Selections = () => (
     return (
       <div>
-        <select>
+        <select id="noun1">
+          <option></option>
+        </select>
+        <select id="verb">
+          <option></option>
+        </select>
+        <select id="noun2">
+          <option></option>
+        </select>
+        <select id="adj">
           <option></option>
         </select>
       </div>
@@ -31,9 +41,12 @@ class MadLibs extends React.Component {
   //handle wordSelect
 
   //handleGenerateMadlib
+  //This is a button that will generate the madLib with chosen words(held in state)
+  //and render the completed madlib to the page
+
 
   render() {
-    return(
+    return (
       <div>
         <h1>madLibsC component rendered here</h1>
       </div>
