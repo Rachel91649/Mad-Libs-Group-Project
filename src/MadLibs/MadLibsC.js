05eada4d28cbe795ff19e717d13623d22b31dca2
jullie0608 <blema90.jl@gmail.com>
2018-01-14 13:01:02 -0500

    Julissa Mad LibC

---
+++ src/MadLibs/MadLibsC.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MadLibsC = ({ }) => (
+  <div>
+    <h1> A Sweet and Sour Past </h1>
+
+    <h2>The Story That Starts Sour And Ends Sweet ! </h2>
+
+    <p> `Sometimes, you don't only need to use your ${} to know how to {}
+      a thing but you need your ${} to see if it's  ${}` </p>
+
+  </div>
+)
+
+export default MadLibsC;
