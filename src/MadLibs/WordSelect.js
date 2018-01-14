bdf4a9c8fd343556e19358ef0b2e40d1f96428d6
Rachel Jackson <rachelnjackson@yahoo.com>
2018-01-14 15:53:09 -0500

    Mike found the Bug!! changed onChage to onChange in WordSelect.js

--- src/MadLibs/WordSelect.js
+++ src/MadLibs/WordSelect.js
@@ -3,22 +3,22 @@ import React from 'react';
 const WordSelect = ({nouns, verbs, adjs, onChange}) => (
 
   <div>
-    <select id="noun1" onChage={onChange}>
+    <select id="noun1" onChange={onChange}>
       {nouns.map(noun => (
         <option name="noun1" value={noun}>{noun}</option>
       ))} 
     </select>
-    <select id="verb" onChage={onChange}>
+    <select id="verb" onChange={onChange}>
       {verbs.map(verb =>(
         <option value={verb}>{verb}</option>
       ))}
     </select>
-    <select id="noun2" onChage={onChange}>
+    <select id="noun2" onChange={onChange}>
       {nouns.map(noun => (
         <option value={noun}>{noun}</option>
       ))} 
     </select>
-    <select id="adj" onChage={onChange}>
+    <select id="adj" onChange={onChange}>
       {adjs.map(adj => (
         <option value={adj}>{adj}</option>
       ))}
