282f97d5f799f54331f218c5537134d81caeecfa
Rachel Jackson <rachelnjackson@yahoo.com>
2018-01-14 12:08:10 -0500

    updated madLibs.js

---
+++ src/MadLibs/MadLibs.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+
+class MadLibs extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nouns: [],
+      verbs: [], 
+      adject: []
+    };
+  }
+
+
+  //componentDidMount
+    //This will get the words from API calls
+
+  //handle wordSelect
+
+  //handleGenerateMadlib
+
+  render() {
+    return(
+      <div>
+        <h1>madLibsC component rendered here</h1>
+      </div>
+    );
+  }
+}
